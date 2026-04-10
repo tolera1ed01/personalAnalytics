705ce1a6c192837bf6ce432140ab7457e9ddedbb
tolera1ed01 <tolera1ed@gmail.com>
2026-04-10 18:08:19 +0100

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script";
 import { Analytics } from "@vercel/analytics/next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -40,6 +41,7 @@ export default function RootLayout({
         `}</Script>
         {children}
         <Analytics />
+        <GoogleAnalytics gaId="G-2WFN871WMM" />
       </body>
     </html>
   );
