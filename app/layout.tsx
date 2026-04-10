9496c7f7fa54f63ecf976266245bc55791d934ae
tolera1ed01 <tolera1ed@gmail.com>
2026-04-10 04:10:24 +0100

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -38,6 +39,7 @@ export default function RootLayout({
           })()
         `}</Script>
         {children}
+        <Analytics />
       </body>
     </html>
   );
